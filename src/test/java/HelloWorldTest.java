f9dba21cee1558a0f7e9c59d484d208c9395d30d
ZhouSusan <szhou089@gmail.com>
2022-10-28 16:07:30 -0700

    created unit test

---
+++ src/test/java/HelloWorldTest.java
@@ -0,0 +1,12 @@
+import org.example.HelloWorld;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class HelloWorldTest {
+    @Test
+    public void getName() throws Exception {
+        HelloWorld susan = new HelloWorld("Susan");
+        assertEquals("Susan",susan.getName());
+    }
+}
